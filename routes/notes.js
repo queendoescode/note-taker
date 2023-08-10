2088bd04f799fac3e59357dacba0ea9949ec035f
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-08-09 22:52:32 -0400

    added routes for /notes and wildcard, and began implementing the notes API using fs module.

--- routes/notes.js
+++ routes/notes.js
@@ -1,20 +1,26 @@
 const notes = require('express').Router();
+const fs = require('fs');
+
 //const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req, res) => {
-  readFromFile('./db/notes.json')
-    .then( fileData => {
-      res.send(fileData);
-    })
-    .catch( err => {
-      res.send('Error');
+  // TODO: Implement using fs module
+  fs.readFile(
+    'db/db.json', // path to the notes "database" file
+    {}, // options
+    (err, fileData) => {
+      if (err) {
+        res.sendStatus(500); // "internal server error"
+      } else {
+        // success - we have data
+        const json = JSON.parse(fileData);
+        res.send(json);
+      }
     });
 });
 
 notes.post('/', (req, res) => {
-  console.log(req.body)
-  readAndAppend(req.body, './db/notes.json');
-  res.send('OK');
+  // TODO: Implement using fs module
 });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
